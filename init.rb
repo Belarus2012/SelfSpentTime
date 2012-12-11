71b578a7f619205fd073e48e0853daece176d1b6
Vasily Gotovko <vasily.gotovko@gmail.com>
2012-12-11 13:12:56 +0300

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_own_time_entries do
+  name 'Redmine Own Time Entries plugin'
+  author '//Twinslash'
+  description 'Plugin to show users only own time entries in a project'
+  version '0.0.1'
+  url ''
+  author_url 'http://twinslash.com'
+end
