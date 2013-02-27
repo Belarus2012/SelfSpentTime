1faeb29e815ee825b087e5b8721beab3e84c5548
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-02-27 16:58:24 +0300

    Improve patch of Application#authorize

--- lib/redmine_own_time_entries/application_controller_patch.rb
+++ lib/redmine_own_time_entries/application_controller_patch.rb
@@ -2,32 +2,30 @@ module OwnTimeEntries
   module ApplicationControllerPatch
 
     def self.included(base)
+      base.send(:include, InstanceMethods)
+
       base.class_eval do
         unloadable
 
-        # override method to change authorization logic
-        # Authorize the user for the requested action
-        def authorize(ctrl = params[:controller], action = params[:action], global = false)
-          allowed = User.current.allowed_to?({:controller => ctrl, :action => action}, @project || @projects, :global => global)
-            # =========== patch start ===========
-            # if it is not allowed but ctrl/action = timelog/index
-            # and view_only_own_time_entries is enabled for user then allow user to open timelog page
-            if !allowed && (ctrl == "timelog" && ["index", "report"].include?(action))
-              allowed = User.current.allowed_to?(:view_only_own_time_entries, @project || @projects, :global => true)
-            end
-            # =========== patch end ===========
-          if allowed
-            true
-          else
-            if @project && @project.archived?
-              render_403 :message => :notice_not_authorized_archived_project
-            else
-              deny_access
-            end
-          end
-        end
-
+        alias_method_chain :authorize, :own_time_entries
       end
+    end
+
+    module InstanceMethods
+
+      def authorize_with_own_time_entries(ctrl = params[:controller], action = params[:action], global = false)
+        # if ctrl/action = timelog/index
+        # and view_only_own_time_entries is enabled for user then allow user to open timelog page
+        if (ctrl == "timelog" &&
+            ["index", "report"].include?(action) &&
+            User.current.allowed_to?(:view_only_own_time_entries, @project || @projects, :global => true))
+
+          true
+        else
+          authorize_without_own_time_entries(ctrl, action, global)
+        end
+      end
+
     end
   end
 end
